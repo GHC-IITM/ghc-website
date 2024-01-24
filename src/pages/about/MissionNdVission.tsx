b040a08303dcdb3ba4b1eeccf39b8ba1c4528c8f
lorstenoplo <nishanthdipali@gmail.com>
2024-01-24 16:58:43 +0530

    fixed scroll

--- src/pages/about/MissionNdVission.tsx
+++ src/pages/about/MissionNdVission.tsx
@@ -3,8 +3,13 @@ import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import MissionNdvalues from "../../components/MissionNdValues";
 import TrackDetails from "../../components/TrackDetails";
+import { useEffect } from "react";
 
 export default function Simple() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <Box maxWidth={"full"} px={0}>
       <Navbar />
